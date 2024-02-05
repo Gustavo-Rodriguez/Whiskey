beec488c4196f6eb681d0f484a5396df2f1fb0ac
Gustavo Rodriguez <gustavo.rodriguez@gmail.com>
2024-02-05 10:58:19 -0500

    Cosmetic Changes

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Login extends React.Component {
+	render() {
+        return (
+            <div>
+                login content goes here
+            </div>
+        )
+
+    }
+}
+
+
+export default Login;
