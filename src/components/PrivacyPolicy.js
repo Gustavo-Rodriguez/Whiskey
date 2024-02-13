4edc207ed67756b67d1803f00a747a6051979256
Gustavo Rodriguez <gustavo.rodriguez@gmail.com>
2024-02-13 13:17:34 -0500

    Update to privacy Policy

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -7,7 +7,10 @@ function Privacy() {
 
     <div className='PrivacyPolicy'>
        <br></br>WhiskeyParty App uses your e-mail and name to prevent users from voting on a whiskey more than once.
-       <br></br>We store it in a database that is cleared before each whiskey Party.
+       <br></br>
+       <br></br>We store it in a database WHEN YOU VOTE ON A WHISKEY. 
+       <br></br>We do not store your login unless you vote
+       <br></br>This Database is cleared before each whiskey Party.
        <br></br>Your e-mail may be visibile to other people at the Whiskey Party
        <br></br>Your votes are NOT secret, they will be displayed to all users when Voting is over. 
     </div>
